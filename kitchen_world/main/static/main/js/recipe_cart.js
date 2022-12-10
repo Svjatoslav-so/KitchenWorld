a2f4f431aba03bab58f437de64591210b73d9d96
Alex 0b1001 <korotkov9405co@gmail.com>
2022-12-10 02:08:27 +0200

    Обновленны данные
    Merge

---
+++ kitchen_world/main/static/main/js/recipe_cart.js
@@ -0,0 +1,155 @@
+function CreateRequest()
+{
+    var Request = false;
+
+    if (window.XMLHttpRequest)
+    {
+        //Gecko-совместимые браузеры, Safari, Konqueror
+        Request = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    {
+        //Internet explorer
+        try
+        {
+             Request = new ActiveXObject("Microsoft.XMLHTTP");
+        }    
+        catch (CatchException)
+        {
+             Request = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+    }
+ 
+    if (!Request)
+    {
+        alert("Невозможно создать XMLHttpRequest");
+    }
+    
+    return Request;
+} 
+
+/*
+Функция посылки запроса к файлу на сервере
+r_method  - тип запроса: GET или POST
+r_path    - путь к файлу
+r_args    - аргументы вида a=1&b=2&c=3...
+r_handler - функция-обработчик ответа от сервера
+*/
+function SendRequest(r_method, r_path, r_args, r_handler)
+{
+    //Создаём запрос
+    var Request = CreateRequest();
+    
+    //Проверяем существование запроса еще раз
+    if (!Request)
+    {
+        return;
+    }
+    
+    //Назначаем пользовательский обработчик
+    Request.onreadystatechange = function()
+    {
+        //Если обмен данными завершен
+    if (Request.readyState == 4)
+    {
+        if (Request.status == 200)
+        {
+            //Передаем управление обработчику пользователя
+            // alert("Передаем управление обработчику пользователя")
+            r_handler(Request);
+        }
+        else
+        {
+            //Оповещаем пользователя о произошедшей ошибке
+            alert("Оповещаем пользователя о произошедшей ошибке")
+        }
+    }
+    else
+    {
+        //Оповещаем пользователя о загрузке
+        // alert("Оповещаем пользователя о загрузкее")
+    }
+    }
+    
+    //Проверяем, если требуется сделать GET-запрос
+    if (r_method.toLowerCase() == "get" && r_args.length > 0)
+    r_path += "?" + r_args;
+    
+    //Инициализируем соединение
+    Request.open(r_method, r_path, true);
+    
+    if (r_method.toLowerCase() == "post")
+    {
+        //Если это POST-запрос
+        
+        //Устанавливаем заголовок
+        Request.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=utf-8");
+        //Посылаем запрос
+        Request.send(r_args);
+    }
+    else
+    {
+        //Если это GET-запрос
+        
+        //Посылаем нуль-запрос
+        Request.send(null);
+    }
+} 
+
+
+function Like(filename, elementName)
+{
+    //Создаем функцию обработчик для установки лайка
+    var On = function(Request)
+    {
+        console.log("On");  
+        console.log(Request.responseText);
+        let elems = document.getElementsByName(elementName)
+        elems.forEach(element => {
+            // element.innerHTML = Request.responseText;
+            element.firstElementChild.className += " active";
+            // console.log(element.lastElementChild);
+            // console.log(element.lastElementChild.innerHTML);
+            element.lastElementChild.innerHTML = parseInt(element.lastElementChild.innerHTML)+1;
+            // console.log(element.lastElementChild.innerHTML);
+            element.onclick = function(){
+                Like('/stars_off/', elementName);
+            }
+            // console.log("onclick: ", element.onclick);
+        }); 
+
+    }
+
+     //Создаем функцию обработчик для удаления лайка
+     var Off = function(Request)
+     {
+         console.log("Off");  
+         console.log(Request.responseText);
+         let elems = document.getElementsByName(elementName);
+         elems.forEach(element => {
+             // element.innerHTML = Request.responseText;
+             element.firstElementChild.className = element.firstElementChild.className.replace('active', '');
+            //  console.log(element.lastElementChild);
+            //  console.log(element.lastElementChild.innerHTML);
+             element.lastElementChild.innerHTML = parseInt(element.lastElementChild.innerHTML)-1;
+            //  console.log(element.lastElementChild.innerHTML);
+             element.onclick = function(){
+                 Like('/stars_on/', elementName);
+             }
+            //  console.log("onclick: ", element.onclick); 
+         }); 
+ 
+     }
+     let values = elementName.split("_")
+     console.log(values)
+     let args = `type=${values[0]}&id=${values[1]}`
+    
+    //Отправляем запрос
+    if(filename == '/stars_off/'){
+        SendRequest("GET",filename,args,Off);
+    }
+    else{
+        SendRequest("GET",filename,args,On);
+    }
+    
+} 
---
+++ kitchen_world/main/templatetags/main_tags.py
@@ -0,0 +1,82 @@
+from django import template
+from django.template.loader import render_to_string
+
+from ..models import Category, RecipeComment
+
+register = template.Library()
+
+
+@register.inclusion_tag('main/recipe_cart.html')
+def show_recipe_cart(recipe, r_photo, request):
+    is_liked = False
+    is_bookmark = False
+    if request.user.is_authenticated:
+        liked_recipe = request.user.author.likedrecipe_set.filter(recipe=recipe)
+        is_liked = liked_recipe.filter(liked_type="L").count() > 0
+        is_bookmark = liked_recipe.filter(liked_type="B").count() > 0
+    return {"recipe": recipe, "r_photo": r_photo, "is_liked": is_liked, "is_bookmark": is_bookmark}
+
+
+def is_details_open(selected, hierarchy):
+    # print('SELECTED: ', selected, "\nHIERARHY: ", hierarchy)
+    is_open = False
+    if type(hierarchy) is list:
+        for cat in hierarchy:
+            if type(cat) is list:
+                is_open = is_details_open(selected, cat)
+            else:
+                is_open = cat.name in selected
+            if is_open:
+                return True
+    else:
+        return hierarchy.name in selected
+    return is_open
+
+
+@register.simple_tag()
+def show_categories(selected_categories, cat_list=Category.get_hierarchy()):
+    menu = ''
+    for i in range(len(cat_list)):
+        if i < len(cat_list) - 1 and type(cat_list[i + 1]) is list:
+            menu += f'<li class="menu__item">' \
+                    f'<details'
+            if is_details_open(selected_categories, cat_list[i + 1]):
+                menu += ' open '
+            menu += f'>' \
+                    f'  <summary>' \
+                    f'      <input  name="category" type="checkbox" value="{cat_list[i].name}" '
+            if cat_list[i].name in selected_categories:
+                menu += ' checked '
+            menu += f'>{cat_list[i].name}' \
+                    f'  </summary>'
+        elif type(cat_list[i]) is list:
+            menu += f'  {show_categories(selected_categories, cat_list[i])}' \
+                    f'</details>' \
+                    f'</li>'
+        else:
+            menu += f'<li class="menu__item">' \
+                    f'      <input  name="category" type="checkbox" value="{cat_list[i].name}" '
+            if cat_list[i].name in selected_categories:
+                menu += ' checked '
+            menu += f'>{cat_list[i].name}' \
+                    f'</li>'
+    # print(f'\nStart\n{menu}\nEnd\n')
+    return menu
+
+
+@register.inclusion_tag('main/comment.html')
+def show_comment(comment):
+    return {"comment": comment}
+
+
+@register.simple_tag()
+def show_all_comments(comments, request):
+    html = ""
+    for comment in comments:
+        html += f'<!-- Comment start -->\n{ render_to_string("main/comment.html", {"comment": comment}, request) }' \
+                f'\n <div class="recipe-comment-children">'
+        comment_children = RecipeComment.objects.filter(parent_comment=comment)
+        html += show_all_comments(comment_children, request)
+        html += '\n</div>\n<!-- Comment end -->'
+    return html
+
