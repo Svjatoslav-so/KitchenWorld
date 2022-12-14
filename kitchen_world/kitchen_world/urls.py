10269715cf8f4f21b5ddfdfadc65893fa8750016
Svjatoslav-so <s.svjatoslav.o@gmail.com>
2022-12-14 19:25:14 +0200

    Правки связанные с хостингом

--- kitchen_world/kitchen_world/urls.py
+++ kitchen_world/kitchen_world/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.views.static import serve as mediaserve
+
 
 from django.conf import settings
 
@@ -26,3 +29,12 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Щоб на хостинге робило
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
+
