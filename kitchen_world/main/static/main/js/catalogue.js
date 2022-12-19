e5635389353549713ce28554cf98bb14646b8f5e
Svjatoslav-so <s.svjatoslav.o@gmail.com>
2022-12-19 20:32:02 +0200

    Реализована пагинация на странице каталога
    Также в фильтрах на странице каталога для категорий реализована возможность при активации/деактивации родительской категории автоматически активировать/деактивировать дочерние категории

--- kitchen_world/main/static/main/js/catalogue.js
+++ kitchen_world/main/static/main/js/catalogue.js
@@ -1,3 +1,4 @@
+/*----------------------- Побавление/удаление продукта ------------------------*/
 products_ul = document.getElementById("products_filters");
 
 function addProduct(element){
@@ -17,6 +18,29 @@ function deleteProduct(element){
  element.parentElement.parentElement.removeChild(element.parentElement);
 }
 
+/*-------- Активация/деактивация подкатегорий при клике на родительскую категорию -------*/
+category_ul = document.getElementById("categories_filters");
+
+function childrenLikeParent(){
+    let details = this.closest("details");
+    let detailsInputsAll = details.getElementsByTagName("input");
+    for(let i = 0; i < detailsInputsAll.length; i++){
+        if (this.checked){
+            detailsInputsAll[i].checked = true;
+        }
+        else{
+            detailsInputsAll[i].checked = false;
+        }
+    }
+}
+
+allSummaryInCategoryUl = category_ul.getElementsByTagName('summary');
+for(let i = 0; i < allSummaryInCategoryUl.length; i++){
+    allSummaryInCategoryUl[i].getElementsByTagName('input')[0].onclick = childrenLikeParent;
+}
+
+
+/*----------------------- Очищение всех фильтров ------------------------*/
 function resetAllFilters(element){
     allChecked = element.querySelectorAll("[checked]");
     for(let i=0; i< allChecked.length; i++){
@@ -28,6 +52,7 @@ function resetAllFilters(element){
 
 }
 
+/*----------- Блок посвященный сварачиванию/разворачиванию категорий фильтров   ----------*/
 let exceptionFilter = document.getElementById("menu_exception_filter");
 
 if(exceptionFilter){
@@ -83,6 +108,7 @@ function closeProductFilter(){
     productFilter.getElementsByClassName("filter__item")[0].onclick = openProductFilter;
 }
 
+/*---------------- Блок адаптива меню -----------------*/
 
 let filtersMenu = document.getElementById("filters-menu");
 
@@ -95,3 +121,126 @@ function closeFiltersMenu() {
 }
 
 document.getElementById("close-filters-menu-btn").onclick = closeFiltersMenu;
+
+
+
+/*------------------- Блок функций посвященный пагинации -------------------*/
+function loadNextPage(filename){ 
+    let loadNextPageBtn = document.getElementById("load-next-page-btn");
+    let args = "page_num="+loadNextPageBtn.dataset.next_page+"&"+loadNextPageBtn.dataset.request_args;
+
+    //Создаем функцию обработчик для догрузки карточек
+    var showCarts = function(Request)
+    {
+        json_obj = JSON.parse(Request.response)
+        var cards_wrapper = document.getElementsByClassName('cards-wrapper')[0];
+        cards_wrapper.innerHTML += json_obj.cart_list;
+        if(json_obj.next_page){
+            loadNextPageBtn.dataset.next_page = json_obj.next_page;
+        }
+        else{
+            loadNextPageBtn.parentElement.removeChild(loadNextPageBtn);
+        }
+                
+    }
+
+    SendRequest("GET",filename,args,showCarts);
+}
+
+function CreateRequest()
+{
+    var Request = false;
+
+    if (window.XMLHttpRequest)
+    {
+        //Gecko-совместимые браузеры, Safari, Konqueror
+        Request = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    {
+        //Internet explorer
+        try
+        {
+             Request = new ActiveXObject("Microsoft.XMLHTTP");
+        }    
+        catch (CatchException)
+        {
+             Request = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+    }
+ 
+    if (!Request)
+    {
+        alert("Невозможно создать XMLHttpRequest");
+    }
+    
+    return Request;
+} 
+
+/*
+Функция посылки запроса к файлу на сервере
+r_method  - тип запроса: GET или POST
+r_path    - путь к файлу
+r_args    - аргументы вида a=1&b=2&c=3...
+r_handler - функция-обработчик ответа от сервера
+*/
+function SendRequest(r_method, r_path, r_args, r_handler)
+{
+    //Создаём запрос
+    var Request = CreateRequest();
+    
+    //Проверяем существование запроса еще раз
+    if (!Request)
+    {
+        return;
+    }
+    
+    //Назначаем пользовательский обработчик
+    Request.onreadystatechange = function()
+    {
+        //Если обмен данными завершен
+    if (Request.readyState == 4)
+    {
+        if (Request.status == 200)
+        {
+            //Передаем управление обработчику пользователя
+            // alert("Передаем управление обработчику пользователя")
+            r_handler(Request);
+        }
+        else
+        {
+            //Оповещаем пользователя о произошедшей ошибке
+            alert("Оповещаем пользователя о произошедшей ошибке")
+        }
+    }
+    else
+    {
+        //Оповещаем пользователя о загрузке
+        // alert("Оповещаем пользователя о загрузкее")
+    }
+    }
+    
+    //Проверяем, если требуется сделать GET-запрос
+    if (r_method.toLowerCase() == "get" && r_args.length > 0)
+    r_path += "?" + r_args;
+    
+    //Инициализируем соединение
+    Request.open(r_method, r_path, true);
+    
+    if (r_method.toLowerCase() == "post")
+    {
+        //Если это POST-запрос
+        
+        //Устанавливаем заголовок
+        Request.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=utf-8");
+        //Посылаем запрос
+        Request.send(r_args);
+    }
+    else
+    {
+        //Если это GET-запрос
+        Request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        //Посылаем нуль-запрос
+        Request.send(null);
+    }
+} 
